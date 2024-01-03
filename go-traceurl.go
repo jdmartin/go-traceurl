404d1ae7cd1514b712ca93634c5b84229453b365
jdmartin <jonsaccount+githubjd@icloud.com>
2024-01-02 22:36:24 -0800

    Bump Version

--- go-traceurl.go
+++ go-traceurl.go
@@ -26,7 +26,7 @@ var (
 	showSourceLink = true
 	useCount       int
 	ugcPolicy      = bluemonday.UGCPolicy()
-	Version        = "2023.12.19.1"
+	Version        = "2024.01.02.1"
 )
 
 var allowedEndpoints = map[string]bool{
