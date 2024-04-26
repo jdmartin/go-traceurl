5ce0cca4ea301d1a17190e144edcbf3be4a6ebd4
jdmartin <jonsaccount+githubjd@icloud.com>
2024-04-26 05:03:50 -0700

    Updated DOMPurify

--- go-traceurl.go
+++ go-traceurl.go
@@ -30,7 +30,7 @@ var (
 	showSourceLink = true
 	useCount       int
 	ugcPolicy      = bluemonday.UGCPolicy()
-	Version        = "2024.04.07.1"
+	Version        = "2024.04.26.1"
 )
 
 var allowedEndpoints = map[string]bool{
