aec1027233518807c7090a87bd0abe3ff06e2dc0
jdmartin <jonsaccount+githubjd@icloud.com>
2025-07-08 17:36:01 -0700

    Bumped Go Version, deps

--- go-traceurl.go
+++ go-traceurl.go
@@ -30,7 +30,7 @@ var (
 	showSourceLink = true
 	useCount       int
 	ugcPolicy      = bluemonday.UGCPolicy()
-	Version        = "2025.06.05.1"
+	Version        = "2025.07.08.1"
 )
 
 var allowedEndpoints = map[string]bool{
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module go-traceurl
 
-go 1.24.4
+go 1.24.5
 
 require github.com/microcosm-cc/bluemonday v1.0.27 // direct
 
