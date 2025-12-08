9c7dd5a73444abb396df1a73217c054697e887dc
jdmartin <jonsaccount+githubjd@icloud.com>
2025-12-08 14:51:22 -0800

    Updated DOMPurify

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:alpine AS build
 
-#Build 02 Dec 2025
+#Build 08 Dec 2025
 
 WORKDIR /
 COPY . .
--- go-traceurl.go
+++ go-traceurl.go
@@ -30,7 +30,7 @@ var (
 	showSourceLink = true
 	useCount       int
 	ugcPolicy      = bluemonday.UGCPolicy()
-	Version        = "2025.12.02.1"
+	Version        = "2025.12.08.1"
 )
 
 var allowedEndpoints = map[string]bool{
