afd51db6d275f121a64dc3b24993358e126d802b
jdmartin <jonsaccount+githubjd@icloud.com>
2026-06-02 22:52:06 -0700

    Update Go version

--- go-traceurl.go
+++ go-traceurl.go
@@ -30,7 +30,7 @@ var (
 	showSourceLink = true
 	useCount       int
 	ugcPolicy      = bluemonday.UGCPolicy()
-	Version        = "2026.05.26.1"
+	Version        = "2026.06.02.1"
 )
 
 var allowedEndpoints = map[string]bool{
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module go-traceurl
 
-go 1.26.3
+go 1.26.4
 
 require github.com/microcosm-cc/bluemonday v1.0.27 // direct
 
