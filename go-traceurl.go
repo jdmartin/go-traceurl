94fb879029cbce2a5bb838068a19c9daca525ce7
jdmartin <jonsaccount+githubjd@icloud.com>
2026-08-19 17:14:03 -0700

    Update version

--- go-traceurl.go
+++ go-traceurl.go
@@ -30,7 +30,7 @@ var (
 	showSourceLink = true
 	useCount       int
 	ugcPolicy      = bluemonday.UGCPolicy()
-	Version        = "2026.08.13.1"
+	Version        = "2026.08.19.1"
 )
 
 var allowedEndpoints = map[string]bool{
